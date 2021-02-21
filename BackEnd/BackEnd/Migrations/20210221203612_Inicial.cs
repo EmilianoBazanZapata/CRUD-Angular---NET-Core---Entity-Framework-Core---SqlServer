f61b7442ecfdb8a02536bc49b7f9159b2ba57288
Emiliano Bazan <maximolicanelo@gmail.com>
2021-02-21 21:40:31 +0100

    Migracion De La Base De Datos

---
+++ BackEnd/BackEnd/Migrations/20210221203612_Inicial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackEnd.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TrajetaCredito",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titular = table.Column<string>(type: "varchar(100)", nullable: false),
+                    NumeroTarjeta = table.Column<string>(type: "varchar(16)", nullable: false),
+                    nFechaExpiracio = table.Column<string>(type: "varchar(5)", nullable: false),
+                    Cvv = table.Column<string>(type: "varchar(3)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrajetaCredito", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TrajetaCredito");
+        }
+    }
+}
