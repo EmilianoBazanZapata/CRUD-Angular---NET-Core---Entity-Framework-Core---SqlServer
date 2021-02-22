f04971fa72e609a837fb709acffd803339807eef
Emiliano Bazan <maximolicanelo@gmail.com>
2021-02-22 03:43:17 +0100

    agregando componente : listtarjetacrdito

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
+import { TarjetaCreditoComponent } from './components/tarjetas/tarjeta-credito/tarjeta-credito.component';
+import { ListtarjetacreditoComponent } from './components/tarjetas/listtarjetacredito/listtarjetacredito.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TarjetasComponent
+    TarjetasComponent,
+    TarjetaCreditoComponent,
+    ListtarjetacreditoComponent
   ],
   imports: [
     BrowserModule,
